9add78f82ff097a3f7d494b9a58eb9396c874ca8
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2024-12-06 06:37:49 +0000

    reworked camera following player

--- client/game/classes/Player.js
+++ client/game/classes/Player.js
@@ -1,34 +1,28 @@
 export class Player extends Phaser.Physics.Arcade.Sprite {
 
   constructor(scene, x, y, sprite_name = "player") {
+    
     super(scene, x, y, sprite_name, 0);
     scene.add.existing(this);
     scene.physics.add.existing(this);
     this.setCollideWorldBounds(true);
 
     this.scene = scene;
-
-    this.speed = this.scene.config.player.speed;
-    this.maxSpeed = this.scene.config.player.speed * 3;
     
   }
 
   followMouse(pointer) {
-    const dx = pointer.x - this.x;
-    const dy = pointer.y - this.y;
-    const distance = Math.sqrt(dx * dx + dy * dy);
+    if (!pointer) return;
 
-    if (distance < 5) {
-      this.setVelocity(0);
-      return;
-    }
+    const angle = Phaser.Math.Angle.Between(this.x, this.y, pointer.worldX, pointer.worldY);
+    const distance = Phaser.Math.Distance.Between(this.x, this.y, pointer.worldX, pointer.worldY);
 
-    const normalizedX = dx / distance;
-    const normalizedY = dy / distance;
+    const speed = Math.min(this.scene.config.player.speed * 3, distance * 0.3);
 
-    const speed = Math.min(this.maxSpeed, distance);
+    this.rotation = angle
+    this.scene.physics.velocityFromRotation(angle, speed, this.body.velocity);
 
-    this.setVelocity(normalizedX * speed, normalizedY * speed);
-    this.setRotation(Math.atan2(dy, dx));
+    console.log(this.x, this.y)
   }
+
 }

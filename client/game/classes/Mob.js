914c8d4b7154521cc9a08f051b10e10ab59c022b
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2024-12-06 00:45:04 +0000

    Mob general class and sprite animations

---
+++ client/game/classes/Mob.js
@@ -0,0 +1,26 @@
+export class Mob extends Phaser.Physics.Arcade.Sprite {
+
+  constructor(scene, sprite_name, x, y, anim_config) {
+
+    super(scene, x, y, sprite_name, 0)
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
+    this.setCollideWorldBounds(true)
+
+    this.scene = scene
+
+    this.scene.anims.create({
+      key: `${sprite_name}_anim`,
+      frames: this.scene.anims.generateFrameNumbers(
+        sprite_name, 
+        anim_config
+      ),
+      frameRate: 15,
+      repeat: -1
+    });
+
+    this.play(`${sprite_name}_anim`);
+
+  }
+  
+}
